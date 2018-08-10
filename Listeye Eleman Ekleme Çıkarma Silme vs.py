6eba40d720c4b1c3f815fd87f3223f26e00d7227
SercanSB <36644057+SercanSB@users.noreply.github.com>
2018-08-10 18:48:22 +0300

    Add files via upload

---
+++ Listeye Eleman Ekleme Çıkarma Silme vs.py
@@ -0,0 +1,20 @@
+sesliHarfler=["a","e","ı","i"]
+print(sesliHarfler)
+
+sesliHarfler += ["ö","o"]
+print(sesliHarfler)
+
+sesliHarfler.append("U")
+print(sesliHarfler)
+
+
+sesliHarfler[1:3]=("Ü")
+print(sesliHarfler)
+
+
+sesliHarfler[:1]=("")
+print(sesliHarfler)
+
+
+sesliHarfler.clear()
+print(sesliHarfler)
