8763cacfbb0997a591667b7bf746500dc3db5268
SercanSB <36644057+SercanSB@users.noreply.github.com>
2018-08-10 18:08:32 +0300

    Add files via upload

---
+++ metni büyük küçük harf yazdırma.py
@@ -0,0 +1,5 @@
+istiklal="Korkma sönmez bu şafaklarda yüzen al sancak."
+
+print(istiklal.lower())      #Metnin tamamını küçük harf yapar.
+
+print(istiklal.upper())      #Metnin tamamını büyük harf yapar.
