ec0ba33d46a5a2cda0a374e5443233dad1dce5db
SercanSB <36644057+SercanSB@users.noreply.github.com>
2018-08-11 18:06:17 +0300

    Add files via upload

---
+++ If Elif Else.py
@@ -0,0 +1,22 @@
+name = "Sercan"
+age = 23
+
+
+if name is "Sercan" or  age is 23:    # if name == "sercan" or age == 23   te aynı şeydir..
+    print("Merhaba Sercan")
+    print("Nasılsın")
+#############################################
+
+
+age = 23
+
+
+if age > 23:
+    print("true")
+
+elif age < 23:
+    print("true")
+
+
+else:
+    print("hiç biri doğru değil")
